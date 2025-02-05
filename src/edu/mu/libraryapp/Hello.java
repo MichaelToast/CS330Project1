2b8071a2190c4cd95a5326e57e93f23792a81c14
MichaelToast <michaelp86788@gmail.com>
2025-02-05 10:57:14 -0600

    basic file layout

--- src/edu/mu/libraryapp/Hello.java
+++ src/edu/mu/libraryapp/Hello.java
@@ -1,10 +1,13 @@
-package project.hello;
+package edu.mu.libraryapp;
+
+import edu.mu.libraryapp.book.LibraryBook;
 
 public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World!");
+		LibraryBook book1 = new LibraryBook();
 
 	}
 
---
+++ src/edu/mu/libraryapp/book/LibraryBook.java
@@ -0,0 +1,14 @@
+package edu.mu.libraryapp.book;
+
+public class LibraryBook {
+	private String title;
+	private String author;
+	private String ISBN;
+	private double price; 
+	
+		
+	
+	
+	
+	
+}
