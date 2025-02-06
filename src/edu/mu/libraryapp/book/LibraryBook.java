67711a64e1cb8ba2f4d0371945e951086ae7366f
noahmcallister04 <116388091+noahmcallister04@users.noreply.github.com>
2025-02-06 09:49:02 -0600

    Added a Default constructor "LibraryBook" that initializes title, author, and ISBN to "Unknown" and price to 0.0

--- src/edu/mu/libraryapp/book/LibraryBook.java
+++ src/edu/mu/libraryapp/book/LibraryBook.java
@@ -6,7 +6,16 @@ public class LibraryBook {
 	private String ISBN;
 	private double price; 
 	
-		
+	/**
+	 * Added a Default constructor "LibraryBook" that initializes title, author, and ISBN to "Unknown" and price to 0.0
+	 */
+	public LibraryBook(string title, string author, string ISBN, double price) 
+	{
+		this.title = "Unknown";
+		this.author = "Unknown";
+		this.ISBN = "Unknown";
+		this.price = "0.0";
+	}
 	
 	
 	
