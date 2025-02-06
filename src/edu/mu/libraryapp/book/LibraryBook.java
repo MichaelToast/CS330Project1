e6d92920959cb69a58bb23b50898debca8898b87
Michael Huber <128461719+GitAtMike@users.noreply.github.com>
2025-02-06 13:28:39 -0600

    Fixed default constructor and added parameterized constructor

--- src/edu/mu/libraryapp/book/LibraryBook.java
+++ src/edu/mu/libraryapp/book/LibraryBook.java
@@ -8,16 +8,31 @@ public class LibraryBook {
 	
 	/**
 	 * Added a Default constructor "LibraryBook" that initializes title, author, and ISBN to "Unknown" and price to 0.0
-	 */
-	public LibraryBook(string title, string author, string ISBN, double price) 
+  	*/
+	public LibraryBook() 
 	{
 		this.title = "Unknown";
 		this.author = "Unknown";
 		this.ISBN = "Unknown";
-		this.price = "0.0";
+		this.price = 0.0;
 	}
-	
-	
+		
+		
+	/**
+	* Created a parameterized constructor that allows the user to set custom values
+	* to the parameters.
+	* @param title - The title of the book.
+	* @param author - The author of the book.
+	* @param ISBN - The ISBN number of the book.
+	* @param price - The price of the book.
+	*/
+	public LibraryBook(String title, String author, String ISBN, double price) 
+	{
+		this.title = title;
+		this.author = author;
+		this.ISBN = ISBN;
+		this.price = price;
+	}
 	
 	
 }
